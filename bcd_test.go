4f74bbd3db6982d9eb0e2e71948d8299f0afec7d
Taichi Sasaki <t.sasaki@ebisol.co.jp>
2017-04-11 15:26:07 +0900

    Improve tests

--- bcd_test.go
+++ bcd_test.go
@@ -12,11 +12,11 @@ type encDecTest struct {
 
 var encDecTests = []encDecTest{
 	{[]byte{}, []byte{}},
-	{[]byte{0x01}, []byte{0, 1}},
-	{[]byte{0x01, 0x23}, []byte{0, 1, 2, 3}},
-	{[]byte{0x01, 0x23, 0x45}, []byte{0, 1, 2, 3, 4, 5}},
-	{[]byte{0x01, 0x23, 0x45, 0x67}, []byte{0, 1, 2, 3, 4, 5, 6, 7}},
-	{[]byte{0x01, 0x23, 0x45, 0x67, 0x89}, []byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}},
+	{[]byte{0x01}, []byte{0x00, 0x01}},
+	{[]byte{0x01, 0x23}, []byte{0x00, 0x01, 0x02, 0x03}},
+	{[]byte{0x01, 0x23, 0x45}, []byte{0x00, 0x01, 0x02, 0x03, 0x04, 0x05}},
+	{[]byte{0x01, 0x23, 0x45, 0x67}, []byte{0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07}},
+	{[]byte{0x01, 0x23, 0x45, 0x67, 0x89}, []byte{0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09}},
 }
 
 func TestEncode(t *testing.T) {
@@ -25,11 +25,7 @@ func TestEncode(t *testing.T) {
 		n, err := Encode(dst, test.dec)
 		if err != nil {
 			t.Errorf("#%d: bad return value: got: %d want: %d", i, n, len(dst))
-		}
-		if n != len(dst) {
-			t.Errorf("#%d: bad return value: got: %d want: %d", i, n, len(dst))
-		}
-		if bytes.Equal(dst, test.enc) != true {
+		} else if bytes.Equal(dst, test.enc) != true {
 			t.Errorf("#%d: got: %#v want: %#v", i, dst, test.enc)
 		}
 	}
